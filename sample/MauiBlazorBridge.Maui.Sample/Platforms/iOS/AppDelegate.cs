c058e90e904cf458993aea42688ff5514a09a6ec
Aathif_Mahir <aathifmahir@gmail.com>
2024-06-22 06:38:07 +0530

    MauiBlazorHybrid Sample

---
+++ sample/MauiBlazorBridge.Maui.Sample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace MauiBlazorBridge.Maui.Sample;
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
