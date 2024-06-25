7e041e2d0a3475f44f0d81f385af3e8a57a0cff1
Aathif_Mahir <aathifmahir@gmail.com>
2024-06-25 08:01:09 +0530

    Comments

--- src/MauiBlazorBridge/BridgeContext.cs
+++ src/MauiBlazorBridge/BridgeContext.cs
@@ -20,6 +20,13 @@ public sealed class BridgeContext : IAsyncDisposable
         OnChanged?.Invoke(e);
     }
 
+    /// <summary>
+    /// This is Used for Creating a BridgeContext and Listening to Changes in the Form Factor of the Device
+    /// </summary>
+    /// <param name="bridge">Current Bridge Object</param>
+    /// <param name="onIdiomChangedCallback">Delegate for Notifying the UI</param>
+    /// <returns></returns>
+
     public async static Task<BridgeContext> CreateAsync(IBridge bridge, Action<DeviceFormFactor> onIdiomChangedCallback)
     {
         var bridgeContext = new BridgeContext(bridge);
@@ -32,6 +39,10 @@ public sealed class BridgeContext : IAsyncDisposable
         return bridgeContext;
     }
 
+
+    /// <summary>
+    /// This is Used for Disposing Whole BridgeContext and Stopping Listening to Changes in the Form Factor of the Device
+    /// </summary>
     public async ValueTask DisposeAsync()
     {
         if (!_isGlobalListener)
