3939fa016560e52da5f2d11e3048199f4e2751b0
Aathif_Mahir <aathifmahir@gmail.com>
2024-06-25 07:35:47 +0530

    Sample Fixes

--- sample/MauiBlazorBridge.Blazor.Sample/Program.cs
+++ sample/MauiBlazorBridge.Blazor.Sample/Program.cs
@@ -2,6 +2,7 @@ using MauiBlazorBridge.Blazor.Sample;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MauiBlazorBridge;
+using MauiBlazorBridge.Blazor.Sample.wwwroot.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,6 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.UseMauiBlazorBridge();
+builder.Services.AddMauiBlazorBridge();
 
 await builder.Build().RunAsync();
