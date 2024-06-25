b0e5d956e928603a5c64ca9b307495fc0d7d3c17
Aathif_Mahir <aathifmahir@gmail.com>
2024-06-25 07:34:13 +0530

    Js Update

--- src/MauiBlazorBridge/wwwroot/MauiBlazorBridge.js
+++ src/MauiBlazorBridge/wwwroot/MauiBlazorBridge.js
@@ -39,3 +39,18 @@ export function disposeListeners() {
         window.resizeListener = null;
     }
 }
+
+export function getPlatform() {
+    const userAgent = navigator.userAgent;
+
+    if (userAgent.includes('Windows')) {
+        return 'Windows';
+    } else if (userAgent.includes('Mac OS X')) {
+        return 'Mac';
+    } else if (userAgent.includes('Android')) {
+        return 'Android';
+    } else if (userAgent.includes('iPhone') || userAgent.includes('iPad')) {
+        return 'IOS';
+    }
+    return "Unknown";
+}
