f2b998f1b68938350d00dfafd7c5446d906b662a
Aathif Mahir <aathifmahir@gmail.com>
2025-03-21 07:46:04 +0530

    Refactor IBridge interface and remove unused members
    - Removed `using Microsoft.JSInterop;` and updated namespace.
    - Retained `PlatformVersion`; removed `DeviceFormFactor` and `FormFactorChanged`.
    - Changed `PlatformChanged` from a property to an event.
    - Simplified `InitializeAsync` by removing the `listenerType` parameter.
    - Removed `InitializeListenerAsync` and `DisposeListener` methods.
    - Eliminated `ListenerType` property.

--- src/MauiBlazorBridge/IBridge.cs
+++ src/MauiBlazorBridge/IBridge.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.JSInterop;
-
-namespace MauiBlazorBridge;
+﻿namespace MauiBlazorBridge;
 public interface IBridge
 {
     /// <summary>
@@ -19,40 +17,13 @@ public interface IBridge
     string PlatformVersion { get; }
 
     /// <summary>
-    /// This is Used for Determining the Form Factor of the Device, Whether it is Desktop, Tablet, or Mobile
-    /// </summary>
-    DeviceFormFactor DeviceFormFactor {  get; }
-
-    /// <summary>
-    /// This is Used for Listening to Changes in the Form Factor of the Device and Internally Used by the BridgeContext
-    /// </summary>
-    EventHandler<DeviceFormFactor>? FormFactorChanged { get; set; }
-    /// <summary>
     /// Platform Changed is Mainly Used for Scenario Where PreRendering is Enabled, In Most Cases Use the Platform Property
     /// </summary>
-    EventHandler<PlatformIdentity>? PlatformChanged { get; set; }
+    event EventHandler<PlatformIdentity>? PlatformChanged;
 
     /// <summary>
     /// This is Used for Initializing the Bridge and used Internally on BridgeProvider
     /// </summary>
-    /// <param name="listenerType">
-    /// Determines Whether the Bridge Should Listen to Changes in Globally, Suppressed or None
-    /// </param>
-    Task InitializeAsync(ListenerType listenerType = ListenerType.None);
+    Task InitializeAsync();
 
-    /// <summary>
-    /// This is Used for Initializing the Listener and Used Internally by the BridgeContext
-    /// </summary>
-    /// <returns></returns>
-    Task InitializeListenerAsync(IJSObjectReference? jsObject = null);
-
-    /// <summary>
-    /// This is Used for Disposing the Listener and Used Internally by the BridgeContext
-    /// </summary>
-    ValueTask DisposeListener();
-
-    /// <summary>
-    /// This is used Identity Whether Listener is Attached to the Bridge Globally, Suppressed
-    /// </summary>
-    ListenerType ListenerType { get; }
 }
